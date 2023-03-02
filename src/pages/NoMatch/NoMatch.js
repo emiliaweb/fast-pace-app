6e24eb6100758633852bf3d2814d256979ac199c
Emilia <81705094+emiliaweb@users.noreply.github.com>
2023-03-02 18:26:30 +0600

    Refactor attribute names for React compatibility

--- src/pages/NoMatch/NoMatch.js
+++ src/pages/NoMatch/NoMatch.js
@@ -1,8 +1,8 @@
 const NoMatch = () => {
     return (
-        <div classNameName="container spacer-170">
-            <h1 classNameName="title title--medium">Requested page was <span>not found</span></h1>
-            <a href="#" classNameName="body-text arrow-link">Back to home page</a>
+        <div className="container spacer-170">
+            <h1 className="title title--medium">Requested page was <span>not found</span></h1>
+            <a href="#" className="body-text arrow-link">Back to home page</a>
         </div>
     )
 }
