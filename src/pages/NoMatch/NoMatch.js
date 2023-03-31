0fa021bd4bec76f538817b13598dd6b2af76b5e8
Emilia <81705094+emiliaweb@users.noreply.github.com>
2023-03-31 22:40:25 +0600

    Animate transition

--- src/pages/NoMatch/NoMatch.js
+++ src/pages/NoMatch/NoMatch.js
@@ -1,15 +1,18 @@
+import { motion } from "framer-motion";
+
 import Showcase from "../../components/Showcase/Showcase";
+import params from "../../services/pageTransitionParams";
 
 const NoMatch = () => {
     return (
-        <>
+        <motion.div {...params}>
             <div className="container spacer-170">
                 <h1 className="title title--medium">Requested page was <span>not found</span></h1>
                 <a href="#" className="body-text arrow-link">Back to home page</a>
                 <div className="body-text">Or check out our other products:</div>
             </div>
             <Showcase />
-        </>
+        </motion.div>
     )
 }
 
